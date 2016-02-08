e61306657b2aa5b31e6aa78c712610723a882a91
Billy Wong <bwong5@stuy.edu>
2016-02-07 20:26:14 -0500

    Modified square root checker for better precision

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -12,26 +12,22 @@ public class Recursion implements hw01{
 
 
     public double convergeOnRoot(double n, double guess) {
-	//System.out.println("Guess: " + guess);
-	//System.out.println("Number: " + n);
-	//System.out.println("% Error: " + Math.abs((n-Math.pow(guess,2.0)))/n);
-	//System.out.println(Math.abs((n-Math.pow(guess,2.0))/n) < 0.01);
-	if (Math.abs((n-Math.pow(guess,2.0)))/n < 0.001) {
-	    //System.out.println("found root");
+	if (Math.abs((n-Math.pow(guess,2.0)))/n < 0.00001) {
 	    return guess;
 	}
 	guess = (n/guess+guess)/2;
-	//System.out.println(guess);
 	return convergeOnRoot(n,guess);
     }
 
 
     public static void main(String[]args) {
 	Recursion r = new Recursion();
-	System.out.println(r.sqrt(100));
-	System.out.println(r.sqrt(200));
-	System.out.println(r.sqrt(64));
-	System.out.println(r.sqrt(78));
+	//System.out.println(r.sqrt(100));
+	//System.out.println(r.sqrt(200));
+	//System.out.println(r.sqrt(64));
+	//System.out.println(r.sqrt(78));
+	System.out.println(r.sqrt(4.0));
+	System.out.println(r.sqrt(1.0E-7));
     }
 
 }
