9a9d4121d3f37ea7e99f7fbd131a070a77cfcfb4
Billy Wong <bwong5@stuy.edu>
2016-02-11 21:32:15 -0500

    Worked on knightboard

--- 03KnightsJourney/KnightBoard.java
+++ 03KnightsJourney/KnightBoard.java
@@ -1,6 +1,8 @@
 public class KnightBoard {
     int[][]board;
+    int [][] moves = {{-2,1},{-2,-1},{-1,-2},{1,-2},{2,-1},{2,1},{1,2},{-1,2}};
 
+    
     public KnightBoard(int size) {
 	//Make 2 extra outsides for out of bounds
 	board = new int[size+4][size+4];
@@ -12,32 +14,54 @@ public class KnightBoard {
 	for (int i = 0; i < 2; i++) {
 	    for (int j = 0; j < board[i].length; j++) {
 		board[i][j] = -1;
+		board[board.length-1-i][j] = -1;
 	    }
 	}
-	for (int i = board.length-3; i < board.length; i++) {
-	    for (int j = 0; j < board[i].length; j++) {
+	for (int i = 2; i < board.length; i++) {
+	    for (int j = 0; j < 2; j++) {
 		board[i][j] = -1;
+		board[i][board[i].length-1-j] = -1;
 	    }
 	}
     }
 
     public boolean solve() {
-	return solevH(0,0,1);
+	return solveH(2,2,1,0);
     }
 
-    public boolean solveH(int row, int col, int num) {
-	board[row][col] = num;
+    private boolean solveH(int row, int col, int num, int moveSlot) {
+	
+	if (canMoveTo(row,col)) {
+	    board[row][col] = num;
+	    solveH(row+(moves[moveSlot])[0],col+(moves[moveSlot])[1],2,0);
+	}
+	
 	return false;
     }
 
-    public printSolution() {
-	
-    }
 
-    public printBoard() {
+    private boolean canMoveTo(int row, int col) {
+	if (board[row][col] != 0) {
+	    return false;
+	}
+	return true;
+    }
+    
+    public void printSolution() {
 	for (int i = 0; i < board.length; i++) {
 	    for (int j = 0; j < board[i].length; j++) {
-		System.out.println(board[i][j] + " ");
+		if (board[i][j] != -1) {
+		    System.out.print(board[i][j] + " ");
+		}
+	    }
+	    System.out.println("\n");
+	}
+    }
+
+    public void printBoard() {
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		System.out.print(board[i][j] + " ");
 	    }
 	    System.out.println("\n");
 	}
@@ -46,6 +70,8 @@ public class KnightBoard {
     public static void main(String[]args) {
 	KnightBoard b = new KnightBoard(4);
 	b.printBoard();
+	System.out.println("\n");
+	b.printSolution();
     }
 
 }
