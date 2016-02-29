c4cced658a53f21e57e86d383a93bb537ffd4641
Billy Wong <bwong5@stuy.edu>
2016-02-29 16:08:22 -0500

    silver usaco start

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -2,30 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Silver {
+
+    int field[][];
+    int steps;
+    int r1;
+    int c1;
+    int r2;
+    int c2;
     
-    public int cowTravel() {
-	String[][]field;
+    public Silver() {
 	Scanner s = new Scanner(new File("ctravel.in"));
 	int rows = s.nextInt();
 	int cols = s.nextInt();
-	int steps = s.nextInt();
+	steps = s.nextInt();
+	int total = 0;
 	field = new String[rows][cols];
 	for (int i = 0; i < rows; i++) {
 	    for (int j = 0; j < cols; j++) {
-		field[i][j] = s.nextInt();
+	        if (s.next().equals("*")) {
+		    field[i][j] = -1;
+		}
+		else {
+		    field[i][j] = 0;
+		}
 	    }
 	}
-	int r1 = s.nextInt();
-	int c1 = s.nextInt();
-	int r2 = s.nextInt();
-	int c2 = s.nextInt();
-	int ways = wanderAround(r1,c1,r2,c2,steps);
-	//6 steps = 6 * big number that depends on # of steps
+	r1 = s.nextInt();
+	c1 = s.nextInt();
+	r2 = s.nextInt();
+	c2 = s.nextInt();
+	field[r1][c1] = 1;
     }
 
 
-    public int wanderAround(int r1,int c1,int r2,int c2, int steps) {
-	
+    public int solve() {
+	for (int i = 0; i < steps; i++) {
+	    //ask every space if it is greater than 0 then add the number to all 
+	    //possible nearby
+	    //make marker to indicate that those new numbers cannot be dealt with
+	    
+	}
+	return field[r2][c2];
     }
 
     public static void main(String[]args) {
