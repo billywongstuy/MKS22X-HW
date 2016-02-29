405761157045ee898d195b5c7cce1f11c901f767
Billy Wong <bwong5@stuy.edu>
2016-02-29 17:57:40 -0500

    Finished silver

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -11,27 +11,33 @@ public class Silver {
     int c2;
     
     public Silver() {
-	Scanner s = new Scanner(new File("ctravel.in"));
-	int rows = s.nextInt();
-	int cols = s.nextInt();
-	steps = s.nextInt();
-	int total = 0;
-	field = new String[rows][cols];
-	for (int i = 0; i < rows; i++) {
-	    for (int j = 0; j < cols; j++) {
-	        if (s.next().equals("*")) {
-		    field[i][j] = -1;
-		}
-		else {
-		    field[i][j] = 0;
+	try {
+	    Scanner s = new Scanner(new File("ctravel.in"));
+	    int rows = s.nextInt();
+	    int cols = s.nextInt();
+	    steps = s.nextInt();
+	    int total = 0;
+	    field = new int[rows][cols];
+	    for (int i = 0; i < rows; i++) {
+	        String next = s.next();
+		for (int j = 0; j < next.length(); j++) {
+		    if (next.charAt(j) == '*') {
+			field[i][j] = -1;
+		    }
+		    else {
+			field[i][j] = 0;
+		    }
 		}
 	    }
+	    r1 = s.nextInt()-1;
+	    c1 = s.nextInt()-1;
+	    r2 = s.nextInt()-1;
+	    c2 = s.nextInt()-1;
+	    field[r1][c1] = 1;
 	}
-	r1 = s.nextInt();
-	c1 = s.nextInt();
-	r2 = s.nextInt();
-	c2 = s.nextInt();
-	field[r1][c1] = 1;
+	catch (FileNotFoundException e) {
+
+	}
     }
 
 
@@ -40,12 +46,67 @@ public class Silver {
 	    //ask every space if it is greater than 0 then add the number to all 
 	    //possible nearby
 	    //make marker to indicate that those new numbers cannot be dealt with
+	    for (int j = 0; j < field.length; j++) {
+		for (int k = 0; k < field[j].length; k++) {
+		    if (field[j][k] > 0) {
+			neighborsSet(j,k);
+		    }
+		}
+	    }
+	    sweepNegatives();
+	    //System.out.println(this);
 	    
 	}
+	//System.out.println(r2 + " " + c2);
 	return field[r2][c2];
     }
 
-    public static void main(String[]args) {
+    
+    public void neighborsSet(int row, int col) {
+	if (col > 0 && (field[row][col-1] >= 0 || field[row][col-1] <= -2)) {
+	    field[row][col-1] += -2*field[row][col];
+	    //System.out.println("left");
+	}
+	if (col < field[0].length-1 && (field[row][col+1] >= 0 || field[row][col+1] <= -2)) {
+	    field[row][col+1] += -2*field[row][col];
+	    //System.out.println("down");
+	}
+	if (row > 0 && (field[row-1][col] >= 0 || field[row-1][col] <= -2)) {
+	    field[row-1][col] += -2*field[row][col];
+	    //System.out.println("up");
+	}
+	if (row < field.length-1 && (field[row+1][col] >= 0 || field[row+1][col] <= -2)) {
+	    field[row+1][col] += -2*field[row][col];
+	    //System.out.println("right");
+	}
+	field[row][col] = 0;
+    }
 
+
+    public String toString() {
+	String str = "";
+	for (int i = 0; i < field.length; i++) {
+	    for (int j = 0; j < field[i].length; j++) {
+		str += (field[i][j] + " ");
+	    }
+	    str += "\n";
+	}
+	return str;
+    }
+
+    public void sweepNegatives() {
+	for (int i = 0; i < field.length; i++) {
+	    for (int j = 0; j < field[i].length; j++) {
+		if (field[i][j] < -1) {
+		    field[i][j] = -1*(field[i][j]/2);
+		}
+	    }
+	}
+    }
+
+    public static void main(String[]args) {
+	Silver s = new Silver();
+	//System.out.println(Arrays.deepToString(s.field));
+	System.out.println(s.solve());
     }
 }
