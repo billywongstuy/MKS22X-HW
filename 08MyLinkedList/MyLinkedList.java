12a57903b589c779e840c0c3fa8f5ffabb84b858
Billy Wong <bwong5@stuy.edu>
2016-03-15 17:50:53 -0400

    Significant work on mylinkedlist

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -1,29 +1,16 @@
 public class MyLinkedList {
 
-    public class LNode {
+    private class LNode {
 	int data;
 	LNode next;	
 
-	public LNode() {
+	public LNode() {}
 
-	}
-
-	public LNode getNext() {
-	    return next;
-	}
-
-	public int getValue() {
-	    System.out.println("V: " + data);
-	    return data;
-	}
-	
-	public void setValue(int value) {
-	    data = value;
-	}
-
-
+	public LNode getNext() {return next;}
+	public int getValue() {return data;}
+	public void setValue(int value) {data = value;}
+	public void setNext(LNode l) {next = l;}
     }
-
 
     LNode start;
     int size;
@@ -35,34 +22,119 @@ public class MyLinkedList {
 
     public boolean add(int value) {
 	LNode current = start;
-	System.out.println("First current: " + current);
-
-	//how do i reassign the value right before it moves to the next inner Lnode
-	while (current != null) {
+	while (current.getNext() != null) {
+	    if (current.getNext().getNext() == null) {
+		current.getNext().setValue(value);
+	    }
 	    current = current.getNext();
 	}
-	//System.out.println("FFFF");
-	System.out.println("Current now: " + current);
-	current = new LNode();
-	System.out.println("Rewriten current: " + current);
-	current.setValue(value);
-	//System.out.println("Value curr: " + current.getValue());
+	if (size == 0) {
+	    start.setValue(value);
+	}
+	
+	current.setNext(new LNode());
 	size++;
 	return true;
     }
 
+     public boolean add(int index, int value) {
+	if (index >= size) {
+	    add(value);
+	}
+	else {
+	    LNode current = start;
+	    for (int i = 0; i < index; i++) {
+		current = current.getNext();
+	    }
+	    add(get(size-1));
+	    for (int i = size-1; i > index; i--) {
+		set(i,get(i-1));
+	    }
+	    current.setValue(value);
+	}
+	return true;
+    }
+    
     public int get(int index) {
-	return 0;
+	if (index >= size) {
+	    throw new IndexOutOfBoundsException();
+	}
+	LNode current = start;
+	for (int i = 0; i < size; i++) {
+	    if (i == index) {
+		return current.getValue();
+	    }
+	    else {
+		current = current.getNext();
+	    }
+	}
+	return -1*Integer.MAX_VALUE;
+    }
+
+    public int size() {
+	return size;
     }
 
 
+    public int set(int index, int newValue) {
+	if (index >= size) {
+	    throw new IndexOutOfBoundsException();
+	}
+	LNode current = start;
+	for (int i = 0; i < index; i++) {
+	    current = current.getNext();
+	}
+	int oldValue = current.getValue();
+	current.setValue(newValue);
+	return oldValue;
+    }
+    
+    //
+    public int remove(int index) {
+	if (index >= size) {
+	    throw new IndexOutOfBoundsException();
+	}
+	LNode current = start;
+	int h;
+	for (h = 0; h < index; h++) {
+	    current = current.getNext();
+	}
+	int removed = get(index);
+	for (int i = h; i < size-1; i++) {
+	    set(i,get(i+1));
+	    current = current.getNext();
+	}
+	current = null;
+	size--;
+	return removed;
+    }
+
+    public int indexOf(int value) {
+	LNode current = start;
+	for (int i = 0; i < size; i++) {
+	    if (current.getValue() == value) {
+		return i;
+	    }
+	    else {
+		current = current.getNext();
+	    }
+	}
+	return -1;
+    }
+    
     public String toString() {
 	String s = "[";
 	LNode current = start;
-	System.out.println("Value c: " + current.getValue());
-	while (current != null) {
-	    s += (current.getValue()+",");
+	int n = 0;
+	while (n < size) {
+	    if (n == size-1) {
+		s += current.getValue();
+	    }
+	    else {
+		s += (current.getValue()+",");
+	    }
 	    current = current.getNext();
+	    n++;
 	}
 	s += "]";
 	return s;
@@ -73,6 +145,19 @@ public class MyLinkedList {
 	System.out.println(l);
 	l.add(4);
 	System.out.println(l);
+        l.add(8);
+	System.out.println(l);
+	l.add(9);
+	l.add(7);
+	l.add(12);
+	System.out.println(l);
+	System.out.println(l.indexOf(9));
+	System.out.println(l.get(3));
+	System.out.println(l.set(2,99));
+	System.out.println(l);
+	l.add(3,23);
+	System.out.println(l);
+	System.out.println(l.remove(2));
 	System.out.println(l);
     }
 
