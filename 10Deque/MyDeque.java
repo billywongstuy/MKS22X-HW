6dc2b8204b627acbd2193c47b1f56fb07db3c263
Billy Wong <bwong5@stuy.edu>
2016-04-01 12:56:30 -0400

    preliminary constructor, add, remove, and resize methods

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+public class MyDeque<T> {
+
+    private Object [] data;
+    private int start;
+    private int end;
+    private int filled;
+
+    public MyDeque() {
+	data = new Object[10];
+	start = 0;
+	end = 0;
+	System.out.println(data.size);
+    }
+
+
+    private void resize() {
+	Object [] bigger = new Object[data.size*2];
+	int arrayPlace = start;
+	
+	for (int i = 0; i < data.size; i++) {
+	    bigger[i] = data[arrayPlace];
+	    if (arrayPlace == data.size-1) {
+		arrayPlace = 0;
+	    }
+	    else {
+		arrayPlace++;
+	    }
+	}
+	start = 0;
+	end = data.size-1;
+	data = bigger;
+    }
+
+    //0. You need a private method to grow the array and copy over the values.
+
+    //There are 6 public methods:
+    //1. 
+    void addFirst(T value) {
+	if (isFull) {
+	    resize;
+	}
+	if (data.size = 0) {
+	    data[0] = value;
+	}
+	else {
+	    if (start == 0) {
+		start = data.size-1;
+	    }
+	    else {
+		start--;
+	    }
+	    data[start] = value;
+	    filled++;
+	}
+    }
+
+    //2. 
+    void addLast(T value) {
+	if (isFull) {
+	    resize;
+	}
+	if (data.size == 0) {
+	    data[0] = value;
+	}
+	else Pif (end == data.size-1) {
+		end = 0;
+	    }
+	else {
+	    end++;
+	}
+	data[end] = value;
+	filled++;
+    }
+}
+//-When the array is full, re-size, then add. 
+//-No exceptions are required since you will re-size.
+
+//3. 
+T removeFirst()  {
+    data[start] = null;
+    filled--;
+    if (start = data.size-1) {
+	start = 0;
+    }
+    else {
+	start++;
+    }
+}
+//4. 
+T removeLast() {
+    data[end] = null;
+    filled--;
+    if (end == 0) {
+	end = data.size-1;
+    }
+    else {
+	end--;
+    }
+} 
+//-NoSuchElementException is thrown when there are no elements. 
+
+//5. 
+T getFirst() {}
+//6. 
+T getLast() {}
+//-NoSuchElementException is thrown when there are no elements. 
+
+public boolean isFull() {
+    return filled = data.size;
+}
+
+public static void main(String[]args) {
+    MyDeque<Integer> d = new MyDeque<>();
+	
+	
+}
+
+}
