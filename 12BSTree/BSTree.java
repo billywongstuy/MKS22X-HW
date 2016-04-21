c399f2674313cd094c265e9a2b2331007c459897
Billy Wong <bwong5@stuy.edu>
2016-04-20 21:52:33 -0400

    more bstree stuff

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -1,55 +1,77 @@
-public class BSTree<T extends Comparable<T>> {
-
-
-    public int compareTo(BSTree n) {
-	if (this.data > n.data) {
-	    return 0;
-	}
-	return 1;
+public class BSTree<T extends Comparable<T>> {   
+    
+    
+    public BSTree() {
+	root = new Node();
     }
 
-    private class Node {
-
+    private class Node{
 	T data;
 	Node left;
 	Node right;
+	int height;
+	// set/get: data/left/right
 
-	//set/get data/left/right
 
-
-	public Node() {
-
+	public int compareTo(T n) {
+	    return n.compareTo(data);
 	}
 
-	public Node(T value) {
-	    
+	
+	public void setValue(T value) {
+	    data = value;
 	}
 
-	public void add(T value) {
-
-	}
-
-
-	public boolean contains(T value) {
-	    return false;
-	}
-
-
-	public int getHeight() {
-	    return 0;
+	public T getValue() {
+	    return data;
 	}
 	
 
+	//real methods here
+	public int height(){ 
+	    return 0;
+	}
+	public void add(T value){
+	    if (data == null) {
+		data = value;
+	    }
+	    else if (left.getValue() != null && right.getValue() != null) {
+		//decide where to put it
+	    }
+	    else if (compareTo(value) == 0) {
+		right.setValue(value);
+	    }
+	    else {
+		left.setValue(value);
+	    }
+	}
+	public String toString(){
+	    return "";
+	}
+	public boolean contains(T value){
+	    return false;
+	}
+     
     }
-    
 
     private Node root;
 
+    //OUTER methods here are wrapper methods for the root
+    public int getHeight(){
+	//call the root's methods
+	//check for empty first!
+	return root.height();
+    }
 
-    //check for empty
-    public void add(T value) {root.add(value);}
-    public String toString() {return root.toString();}
-    public boolean contains(T value) {return root.contains(value);}
-    public int getHeight() {return root.getHeight();}
-
+    public void add(T value){
+	//check for empty before you do things with root.
+    }
+    public String toString(){
+	//check for empty before you do things with root.
+	return "";
+    }
+    public boolean contains(T value){
+	//check for empty before you do things with root.
+	return false;
+    }
 }
